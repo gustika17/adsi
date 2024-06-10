9e24b2c603d5813f2a33fd99f44f7540d009f758
safiiraaulia <safiraaulia286@gmail.com>
2024-06-10 11:47:10 +0700

    up

--- src/PembayarnInsert.php
+++ src/PembayarnInsert.php
@@ -12,26 +12,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = "";
     $dbname = "rentals";
 
-    // Create connection
+    
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
+    
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Prepare and bind
+
     $stmt = $conn->prepare("INSERT INTO rentals (name, phone_number, address, package, rental_date) VALUES (?, ?, ?, ?, ?)");
     $stmt->bind_param("sssss", $nama, $no_telephone, $alamat, $paket_sewa, $tanggal_sewa);
 
-    // Execute the statement
+
     if ($stmt->execute()) {
         echo "New record created successfully";
     } else {
         echo "Error: " . $stmt->error;
     }
 
-    // Close the statement and connection
+
     $stmt->close();
     $conn->close();
 }
